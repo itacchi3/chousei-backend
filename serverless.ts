749795ac64bd263f56c678d4355510c4be16ee76
itaitoshiki <toshiki.i0918@gmail.com>
2022-04-09 22:18:29 +0900

    🎉 init

---
+++ serverless.ts
@@ -0,0 +1,57 @@
+import type { AWS } from "@serverless/typescript";
+
+import hello from "@functions/hello";
+
+const serverlessConfiguration: AWS = {
+  service: "chousei-backend",
+  frameworkVersion: "3",
+  plugins: [
+    "serverless-esbuild",
+    "serverless-offline",
+    "serverless-dotenv-plugin",
+  ],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+    },
+  },
+  // import the function via paths
+  functions: { hello },
+  package: {
+    individually: true,
+    patterns: [
+      "!node_modules/.prisma/client/libquery_engine-*",
+      "!node_modules/.prisma/client/libquery_engine-rhel-*",
+      "!node_modules/.prisma/libquery_engine-*",
+      "!node_modules/@prisma/engines/**",
+    ],
+  },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
+      concurrency: 10,
+    },
+    prune: {
+      automatic: true,
+      number: 3,
+    },
+    serverlessOffline: {
+      httpPort: 8080,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
